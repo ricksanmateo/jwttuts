474bc8377d8e451815dc5942d60a76611fb775a5
rick <ricksanmateo27@gmail.com>
2025-07-27 14:43:51 +0800

    added login

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -37,4 +37,33 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+      email,
+    ]);
+
+    if (user.rows.length === 0) {
+      return res.status(400).json("User does not exist");
+    }
+
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].user_password
+    );
+
+    if (!validPassword) {
+      return res.status(400).json("Invalid Password");
+    }
+
+    const token = jwtGenerator(user.rows[0].user_id);
+    res.json({ token });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 export default router;
